339d7530474649e0518b76e05ebe32b3c8af9c3d
Jason Fiddle <jfiddle@ycp.edu>
2015-05-09 14:42:09 -0400

    Spinner
    Most of what should be in the spinner.  Can’t test on my computer, so
    go for it Cody.

---
+++ Triviagame/app/src/main/java/edu/ycp/cs482/triviagame/CustomOnItemSelectedListener.java
@@ -0,0 +1,18 @@
+package edu.ycp.cs482.triviagame;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+//Handles what happens when an item on the spinner is selected.
+
+public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {}
+}
